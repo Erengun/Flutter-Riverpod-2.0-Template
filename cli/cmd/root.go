e43b866ce37b3325b94ab8f064eea0d3c066ec9b
Eren GÜN <erengun4@gmail.com>
2024-05-28 20:03:17 +0300

    feat: Update CLI tool description and long message

--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -14,14 +14,14 @@ import (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "Flutter-Riverpod-2.0-Template",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "init",
+	Short: "Cli tool to change app name, package name, and other configurations to get started with Flutter Riverpod 2.0 Template.",
+	Long: `
+Cli tool to change app name, package name, and other configurations to get started with Flutter Riverpod 2.0 Template.
+When you run this command, it will ask you for the new app name, package name, and other configurations.
+After you provide the required information, it will update the configurations and rename the files accordingly.
+Write 'init' in the terminal to start the process.
+`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
