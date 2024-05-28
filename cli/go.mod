df37e7ab8c886adc9fac676e1c7dbb4c70aaf758
Eren GÜN <erengun4@gmail.com>
2024-05-28 20:03:02 +0300

    chore: Update module path for CLI package

--- cli/go.mod
+++ cli/go.mod
@@ -1,4 +1,4 @@
-module github.com/Erengun/Flutter-Riverpod-2.0-Template
+module github.com/Erengun/Flutter-Riverpod-2.0-Template/cli
 
 go 1.22.3
 
--- cli/main.go
+++ cli/main.go
@@ -4,7 +4,7 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/Erengun/Flutter-Riverpod-2.0-Template/cmd"
+import "github.com/Erengun/Flutter-Riverpod-2.0-Template/cli/cmd"
 
 func main() {
 	cmd.Execute()
