8c97c07949999fa3cbb46b4de314438f48bbef41
Eren GÜN <erengun4@gmail.com>
2024-05-29 19:50:42 +0300

    chore: Seperate code into functions block in cli

--- cli/cmd/init.go
+++ cli/cmd/init.go
@@ -5,12 +5,178 @@ package cmd
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 	"strconv"
 
+	"github.com/manifoldco/promptui"
+
 	"github.com/spf13/cobra"
 )
+
+type promptContent struct {
+	label string
+	// Optional choices for the select prompt
+	choices []string
+}
+
+func promptGetInput(content string) string {
+	pc := promptContent{label: content}
+	templates := &promptui.PromptTemplates{
+		Prompt:  "{{ . }} ",
+		Valid:   "{{ . | green }} ",
+		Invalid: "{{ . | red }} ",
+		Success: "{{ . | bold }} ",
+	}
+
+	prompt := promptui.Prompt{
+		Label:     pc.label,
+		Templates: templates,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		log.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	log.Printf("Input: %s\n", result)
+
+	return result
+}
+
+func pizzaCursor(input []rune) []rune {
+	return []rune("\u2588")
+}
+
+func promptGetSelect(pc promptContent) string {
+	items := pc.choices
+	index := -1
+	var result string
+	var err error
+
+	for index < 0 {
+		prompt := promptui.Select{
+			Label:   pc.label,
+			Items:   items,
+			Pointer: pizzaCursor,
+		}
+
+		index, result, err = prompt.Run()
+
+		if index == -1 {
+			items = append(items, result)
+		}
+	}
+
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Input: %s\n", result)
+
+	return result
+}
+
+func updateAppNameAndPackageName() {
+	var err error
+	var appName string
+	var packageName string
+	appName = promptGetInput("Enter the new app name (eg: My App). Empty to skip :")
+	packageName = promptGetInput("Enter the new package name (eg: com.example.myapp). Empty to skip :")
+	// flutter pub run rename_app:main all="My App Name"
+	if appName != "" {
+		_, err = exec.Command("flutter", "pub", "run", "rename_app:main", "all="+appName).Output()
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+		log.Println("App name updated successfully.")
+	} else {
+		log.Println("Skipping app name update.")
+	}
+
+	// flutter pub run change_app_package_name:main com.new.package.name
+	if packageName != "" {
+		_, err := exec.Command("flutter", "pub", "run", "change_app_package_name:main", packageName).Output()
+		if err != nil {
+			log.Fatalln(err)
+			return
+		}
+		log.Println("Package name updated successfully.")
+	} else {
+		log.Println("Skipping package name update.")
+	}
+
+	log.Printf("App %s with package name %s updated successfully.\n", appName, packageName)
+}
+
+func updateAppIconAndSplashScreen() {
+	var err error
+	var iconPath string
+	var android bool
+	var ios bool
+
+	// Run flutter pub add flutter_launcher_icons
+	log.Println("Adding flutter_launcher_icons package to the project.")
+	_, err = exec.Command("flutter", "pub", "add", "flutter_launcher_icons").Output()
+	if err != nil {
+		log.Println("Failed to add flutter_launcher_icons package:", err)
+		return
+	}
+	iconPath = promptGetInput("Enter the path to the icon file (eg: icon/path/here.png) : ")
+	ans := promptGetSelect(promptContent{
+		label:   "Do you want to update the android icon? (yes/no): ",
+		choices: []string{"yes", "no"},
+	})
+	if ans == "yes" {
+		android = true
+	} else {
+		android = false
+	}
+	ans = promptGetSelect(promptContent{
+		label:   "Do you want to update the ios icon? (yes/no): ",
+		choices: []string{"yes", "no"},
+	})
+	if ans == "yes" {
+		ios = true
+	} else {
+		ios = false
+	}
+
+	// Add your Flutter Launcher Icons configuration to your pubspec.yaml
+	flutterLauncherIconsConfig := `
+flutter_launcher_icons:
+	image_path_android: ` + iconPath + `
+	image_path_ios: ` + iconPath + `
+	android: ` + strconv.FormatBool(android) + `
+	ios: ` + strconv.FormatBool(ios) + `
+`
+	err = os.WriteFile("pubspec.yaml", []byte(flutterLauncherIconsConfig), 0644)
+	if err != nil {
+		log.Println("Failed to write Flutter Launcher Icons configuration to pubspec.yaml:", err)
+		return
+	}
+	log.Println("Flutter Launcher Icons configuration added to pubspec.yaml.")
+	// flutter pub get
+	log.Println("Running flutter pub get to get the dependencies.")
+	_, err = exec.Command("flutter", "pub", "get").Output()
+	if err != nil {
+		log.Fatal("Failed to run flutter pub get:", err)
+		return
+	}
+
+	// flutter pub run flutter_launcher_icons
+	log.Println("Running flutter pub run flutter_launcher_icons to update the app icon.")
+	_, err = exec.Command("flutter", "pub", "run", "flutter_launcher_icons:main").Output()
+	if err != nil {
+		log.Println("Failed to run flutter pub run flutter_launcher_icons:main:", err)
+		return
+	}
+	log.Println("App icon and splash screen updated successfully.")
+}
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
@@ -22,135 +188,52 @@ It uses the information provided by the user to update the configurations and re
 Write 'template init' in the terminal to start the process.
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
-		// Go to project root directory ../
+		log.Println("init called")
 		// Go to project root directory ../
 		err := os.Chdir("..")
 		if err != nil {
-			fmt.Println("Failed to change directory:", err)
+			log.Println("Failed to change directory:", err)
 			return
 		}
 		// Check if the project is a Flutter project
 		if _, err := os.Stat("pubspec.yaml"); os.IsNotExist(err) {
 			// If not a Flutter project, show error message and exit
-			fmt.Println("Not a Flutter project")
+			log.Println("Not a Flutter project")
 			return
 		}
-		fmt.Println("Flutter project detected. Running flutter pub get to get the dependencies.")
+		log.Println("Flutter project detected. Running flutter pub get to get the dependencies.")
 		// If Flutter project, run flutter pub get to get the dependencies
 		_, err = exec.Command("flutter", "pub", "get").Output()
 		if err != nil {
-			fmt.Println("Failed to run flutter pub get:", err)
+			log.Println("Failed to run flutter pub get:", err)
 			return
 		}
 		// Run flutter pub add rename_app
-		fmt.Println("Adding rename_app package to the project.")
+		log.Println("Adding rename_app package to the project.")
 		_, err = exec.Command("flutter", "pub", "add", "rename_app").Output()
 		if err != nil {
-			fmt.Println("Failed to add rename_app package:", err)
+			log.Println("Failed to add rename_app package:", err)
 			return
 		}
 
-		fmt.Println("Adding change_app_package_name package to the project.")
+		log.Println("Adding change_app_package_name package to the project.")
 		_, err = exec.Command("flutter", "pub", "add", "change_app_package_name", "--dev").Output()
 		if err != nil {
-			fmt.Println("Failed to add change_app_package_name package:", err)
+			log.Println("Failed to add change_app_package_name package:", err)
 			return
 		}
 
-		var appName string
-		fmt.Print("Enter the new app name (eg: My App).\nEmpty to skip : ")
-		fmt.Scanln(&appName)
-		var packageName string
-		fmt.Print("Enter the new package name (com.new.package.name).\nEmpty to skip : ")
-		fmt.Scanln(&packageName)
-		// flutter pub run rename_app:main all="My App Name"
-		if appName != "" {
-			_, err = exec.Command("flutter", "pub", "run", "rename_app:main", "all="+appName).Output()
-			if err != nil {
-				fmt.Println("Failed to run rename_app:main:", err)
-				return
-			}
-			fmt.Println("App name updated successfully.")
-		} else {
-			fmt.Println("Skipping app name update.")
+		updateAppNameAndPackageName()
+
+		ans := promptGetSelect(promptContent{
+			label:   "Do you want to update the app icon and splash screen?: ",
+			choices: []string{"yes", "no"},
+		})
+		if ans == "yes" {
+			updateAppIconAndSplashScreen()
 		}
 
-		// flutter pub run change_app_package_name:main com.new.package.name
-		if packageName != "" {
-			_, err = exec.Command("flutter", "pub", "run", "change_app_package_name:main", packageName).Output()
-			if err != nil {
-				fmt.Println("Failed to run change_app_package_name:main:", err)
-				return
-			}
-			fmt.Println("Package name updated successfully.")
-		} else {
-			fmt.Println("Skipping package name update.")
-		}
-
-		fmt.Printf("App %s with package name %s updated successfully.\n", appName, packageName)
-
-		var ans string
-		for {
-			fmt.Print("Do you want to update the app icon and splash screen? (yes/no): ")
-			fmt.Scanln(&ans)
-			if ans == "yes" {
-				// Run flutter pub add flutter_launcher_icons
-				fmt.Println("Adding flutter_launcher_icons package to the project.")
-				_, err = exec.Command("flutter", "pub", "add", "flutter_launcher_icons").Output()
-				if err != nil {
-					fmt.Println("Failed to add flutter_launcher_icons package:", err)
-					return
-				}
-				fmt.Print("Enter the path to the icon file (eg: icon/path/here.png) : ")
-				var iconPath string
-				fmt.Scanln(&iconPath)
-				var android bool
-				fmt.Print("Do you want to update the android icon? (yes/no): ")
-				fmt.Scanln(&android)
-				var ios bool
-				fmt.Print("Do you want to update the ios icon? (yes/no): ")
-				fmt.Scanln(&ios)
-
-				// Add your Flutter Launcher Icons configuration to your pubspec.yaml
-				flutterLauncherIconsConfig := `
-flutter_launcher_icons:
-	image_path_android: ` + iconPath + `
-	image_path_ios: ` + iconPath + `
-	android: ` + strconv.FormatBool(android) + `
-	ios: ` + strconv.FormatBool(ios) + `
-`
-				err = os.WriteFile("pubspec.yaml", []byte(flutterLauncherIconsConfig), 0644)
-				if err != nil {
-					fmt.Println("Failed to write Flutter Launcher Icons configuration to pubspec.yaml:", err)
-					return
-				}
-				fmt.Println("Flutter Launcher Icons configuration added to pubspec.yaml.")
-				// flutter pub get
-				fmt.Println("Running flutter pub get to get the dependencies.")
-				_, err = exec.Command("flutter", "pub", "get").Output()
-				if err != nil {
-					fmt.Println("Failed to run flutter pub get:", err)
-					return
-				}
-
-				// flutter pub run flutter_launcher_icons
-				fmt.Println("Running flutter pub run flutter_launcher_icons to update the app icon.")
-				_, err = exec.Command("flutter", "pub", "run", "flutter_launcher_icons:main").Output()
-				if err != nil {
-					fmt.Println("Failed to run flutter pub run flutter_launcher_icons:main:", err)
-					return
-				}
-				fmt.Println("App icon and splash screen updated successfully.")
-			} else if ans == "no" {
-				break
-			} else {
-				fmt.Println("Invalid input. Please enter 'yes' or 'no'.")
-			}
-		}
-		// As
-
-		fmt.Println("All configurations updated successfully.")
+		log.Println("All configurations updated successfully.")
 
 	},
 }
--- cli/go.mod
+++ cli/go.mod
@@ -3,10 +3,12 @@ module github.com/Erengun/Flutter-Riverpod-2.0-Template/cli
 go 1.22.3
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
