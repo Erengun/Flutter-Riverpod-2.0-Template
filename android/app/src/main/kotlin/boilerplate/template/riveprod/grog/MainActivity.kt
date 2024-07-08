4ad9b9bbe9de311d5e677f60d361f5f396b2a8fe
Eren GÜN <erengun4@gmail.com>
2024-07-08 10:22:41 +0300

    chore: Update ios, android, macos, windows and web sdk

--- android/app/src/main/kotlin/boilerplate/template/riveprod/grog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package boilerplate.template.riveprod
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/dev/feichtinger/grog/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.feichtinger.template
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
