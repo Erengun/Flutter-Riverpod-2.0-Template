fcff68b6a14ec68252f1804e5f25eea95e5f5452
Eren Gun <erengun4@gmail.com>
2025-02-27 11:54:28 +0300

    feat(endpoints): update baseUrl to use CDN for improved resource loading

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,8 +1,6 @@
 class Endpoints {
   Endpoints._();
-  static const String baseUrl = '';
+  static const String baseUrl = 'https://cdn.statically.io/';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 30000;
-
-  // booking endpoints
 }
