34196b45f1a3758ef48c5ac6df8b30a946ef8fba
Eren Gun <erengun4@gmail.com>
2025-04-15 11:42:40 +0300

    feat(authentication): implement AuthenticationRepository and HttpAuthRepository for user login and registration

--- lib/features/authentication/data/authentication_repository.g.dart
+++ lib/features/authentication/data/authentication_repository.g.dart
@@ -1,29 +1,30 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'abstract_data_repository.dart';
+part of 'authentication_repository.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$remoteDataRepositoryHash() =>
-    r'f469a9c93a034716f226a035a1b0913191689cbe';
+String _$authenticationRepositoryHash() =>
+    r'01262a8bf834bb5296947e0b226fa2055e39554c';
 
-/// See also [remoteDataRepository].
-@ProviderFor(remoteDataRepository)
-final remoteDataRepositoryProvider =
-    AutoDisposeProvider<DataRepository>.internal(
-  remoteDataRepository,
-  name: r'remoteDataRepositoryProvider',
+/// See also [authenticationRepository].
+@ProviderFor(authenticationRepository)
+final authenticationRepositoryProvider =
+    AutoDisposeProvider<AuthenticationRepository>.internal(
+  authenticationRepository,
+  name: r'authenticationRepositoryProvider',
   debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
       ? null
-      : _$remoteDataRepositoryHash,
+      : _$authenticationRepositoryHash,
   dependencies: null,
   allTransitiveDependencies: null,
 );
 
 @Deprecated('Will be removed in 3.0. Use Ref instead')
 // ignore: unused_element
-typedef RemoteDataRepositoryRef = AutoDisposeProviderRef<DataRepository>;
+typedef AuthenticationRepositoryRef
+    = AutoDisposeProviderRef<AuthenticationRepository>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
