2ae7eace59e0537f8831f3bce7a10a84e7f4713f
Eren Gun <erengun4@gmail.com>
2025-04-15 11:42:16 +0300

    feat(endpoints): update base URL and add login/register endpoints

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -1,6 +1,9 @@
 class Endpoints {
   Endpoints._();
-  static const String baseUrl = 'https://cdn.statically.io/';
+  static const String baseUrl = 'https://reqres.in/';
   static const int receiveTimeout = 15000;
   static const int connectionTimeout = 30000;
+
+  static const String login = 'api/login';
+  static const String register = 'api/register';
 }
