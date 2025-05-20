ac6ae711485df7f036caa8898d76574beea18a8e
Eren Gun <erengun4@gmail.com>
2025-05-20 15:27:27 +0300

    feat(authentication): add LocalUserDatasource and generated provider for managing user credentials

---
+++ lib/features/authentication/data/hive/local_user_ds.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'local_user_ds.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$localUserDatasourceHash() =>
+    r'11290a8f4dbe788cdf239a7e913247806690e25e';
+
+/// See also [localUserDatasource].
+@ProviderFor(localUserDatasource)
+final localUserDatasourceProvider =
+    AutoDisposeProvider<LocalUserDatasource>.internal(
+  localUserDatasource,
+  name: r'localUserDatasourceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$localUserDatasourceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef LocalUserDatasourceRef = AutoDisposeProviderRef<LocalUserDatasource>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
