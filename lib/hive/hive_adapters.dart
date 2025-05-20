18771d9a31c36053faa007c94bb4f0f7a32dfa0e
Eren Gun <erengun4@gmail.com>
2025-05-20 15:13:38 +0300

    feat(hive): add Hive adapters for LoginCredentials and ThemeUiModel

---
+++ lib/hive/hive_adapters.dart
@@ -0,0 +1,14 @@
+// lib/hive/hive_adapters.dart   ← keep registrar but REMOVE the spec
+import 'package:flutter/material.dart';
+import 'package:hive_ce/hive.dart';
+
+import '../config/theme/theme_ui_model.dart';
+import '../features/authentication/domain/login_request.dart';
+
+// ignore: always_specify_types
+@GenerateAdapters(firstTypeId: 0,[
+  AdapterSpec<LoginCredentials>(),
+  AdapterSpec<ThemeUiModel>(),
+  // Add other models here
+])           // or just omit the list entirely
+part 'hive_adapters.g.dart';
