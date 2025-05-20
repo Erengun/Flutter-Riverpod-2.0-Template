b3cfe523cc6f4acffbb8681ba68217c8dd38fa4b
Eren Gun <erengun4@gmail.com>
2025-05-20 15:28:37 +0300

    feat(authentication): update RegisterDialog to enhance user registration flow with error handling

--- lib/features/authentication/presentation/register/register_dialog.dart
+++ lib/features/authentication/presentation/register/register_dialog.dart
@@ -3,6 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gap/gap.dart';
 
 import '../../../../utils/context_extensions.dart';
+import '../../domain/register_response.dart';
+import '../login/login_controller.dart';
 
 class RegisterDialog extends ConsumerStatefulWidget {
   const RegisterDialog({super.key});
@@ -31,97 +33,117 @@ class _RegisterDialogState extends ConsumerState<RegisterDialog> {
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.sizeOf(context);
-    return Container(
-      width: size.width * 0.8,
-      height: size.height * 0.5,
-      padding: const EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: context.colorScheme.surface,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Icon(Icons.person, color: context.colorScheme.primary),
-              const SizedBox(width: 10),
-              Text(
-                'New Profile',
-                style: context.textTheme.titleMedium?.copyWith(
-                  color: context.colorScheme.primary,
-                ),
-              ),
-            ],
-          ),
-          const Gap(20),
-          Text('Username',
-              style: context.textTheme.titleMedium?.copyWith(
-                color: context.colorScheme.primary,
-              )),
-          const Gap(10),
-          TextField(
-            controller: _emailController,
-            decoration: InputDecoration(
-              hintText: 'Enter your username',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(
-                  color: context.colorScheme.primary,
-                ),
-              ),
-            ),
-          ),
-          const Gap(20),
-          Text('Password',
-              style: context.textTheme.titleMedium?.copyWith(
-                color: context.colorScheme.primary,
-              )),
-          const Gap(10),
-          TextField(
-            controller: _passwordController,
-            obscureText: true,
-            decoration: InputDecoration(
-              hintText: 'Enter your password',
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(10),
-                borderSide: BorderSide(
-                  color: context.colorScheme.primary,
-                ),
-              ),
-            ),
-          ),
-          const Gap(20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text(
-                  'Cancel',
+    return Dialog(
+      child: Container(
+        width: size.width * 0.8,
+        height: size.height * 0.5,
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Icon(Icons.person, color: context.colorScheme.primary),
+                const SizedBox(width: 10),
+                Text(
+                  'New Profile',
                   style: context.textTheme.titleMedium?.copyWith(
                     color: context.colorScheme.primary,
                   ),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle registration logic here
-                },
-                child: Text(
-                  'Register',
-                  style: context.textTheme.titleMedium?.copyWith(
-                    color: context.colorScheme.onPrimary,
+              ],
+            ),
+            const Gap(20),
+            Text('Username',
+                style: context.textTheme.titleMedium?.copyWith(
+                  color: context.colorScheme.primary,
+                )),
+            const Gap(10),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                hintText: 'Enter your username',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: context.colorScheme.primary,
                   ),
                 ),
               ),
-            ],
-          ),
-        ],
+            ),
+            const Gap(20),
+            Text('Password',
+                style: context.textTheme.titleMedium?.copyWith(
+                  color: context.colorScheme.primary,
+                )),
+            const Gap(10),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Enter your password',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: BorderSide(
+                    color: context.colorScheme.primary,
+                  ),
+                ),
+              ),
+            ),
+            const Gap(20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    'Cancel',
+                    style: context.textTheme.titleMedium?.copyWith(
+                      color: context.colorScheme.primary,
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    ref
+                        .read(loginControllerProvider.notifier)
+                        .register(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        )
+                        .then((RegisterResponse value) {
+                      Navigator.pop(context);
+                    }).catchError((error) {
+                      // Handle error
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text(error.toString()),
+                        ),
+                      );
+                    });
+                  },
+                  child: Text(
+                    'Register',
+                    style: context.textTheme.titleMedium?.copyWith(
+                      color: context.colorScheme.onPrimary,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+/*
+In app update
+*/
