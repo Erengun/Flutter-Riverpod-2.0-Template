63ccbf4ec77d81345870d2d4b9e3aea6920d637b
Eren Gun <erengun4@gmail.com>
2025-05-20 15:10:25 +0300

    feat(endpoints): add apiKey constant for environment configuration

--- lib/constants/endpoints.dart
+++ lib/constants/endpoints.dart
@@ -6,4 +6,10 @@ class Endpoints {
 
   static const String login = 'api/login';
   static const String register = 'api/register';
+
+  //! Dont put api key on the client side. Do it on the server side
+  //! This is just for testing purposes
+  //! and should not be used in production
+  // https://reqres.in/signup
+  static const String apiKey = String.fromEnvironment('API_KEY', defaultValue: 'reqres-free-v1');
 }
